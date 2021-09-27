12afdd0a77e95cfe4f1e93f14432e81d6ecddb8e
Mattéo Legagneux <matteoo34@icloud.com>
2021-09-27 15:46:37 +0200

    Single JS file

--- main.js
+++ main.js
@@ -6,10 +6,11 @@ const Game = {
 		let x = 0, y = 336;
 		for (let i = 0; i < lvl.length; i++) {
 			for (let j = 0; j < lvl[i].length; j++) {
-				if (lvl[i][j] !== undefined) {
+				if (lvl[i][j] !== 0) {
 					// ignores a blocks
 					let block = document.createElement("div");
-					block.className = `block ${lvl[i][j]} ${lvl[i][j] + i + j}`;
+					block.className = `block ${lvl[i][j]}`;
+					if (!(lvl[i][j].includes("behind"))) block.classList.add(lvl[i][j] + i + j);
 					block.style.bottom = `${y + 96}px`;
 					block.style.left = `${x}px`;
 					map.appendChild(block)
@@ -112,26 +113,31 @@ const Game = {
 	togglePauseMenu: function() {
 		if (pause.style.visibility === "hidden") {
 			// showing pause menu, blocking player control
+			paused = true;
 			Game.freeze();
-			// console.log(player.classList)
-			player.classList.add("paused");
 			pause.style.visibility = "visible"
 		} else {
 			// hiding pause menu, allowing player control
-			Game.unfreeze();
-			// console.log(player.classList)
-			player.classList.remove("paused");
+			paused = false;
+			if (spawned) Game.unfreeze();
 			pause.style.visibility = "hidden"
 		}
 	},
-	unfreeze: function() {if (!canLoop) canLoop = window.requestAnimationFrame(Game.loop)},
+	unfreeze: function() {
+		// enable controls
+		if (!canLoop) {
+			document.addEventListener("keydown", Game.keydown);
+			document.addEventListener("keyup", Game.keyup);
+			canLoop = window.requestAnimationFrame(Game.loop)
+		}
+	},
 	loop: function() {
 		canLoop = undefined;
-		document.addEventListener("keydown", Game.keydown);
-		document.addEventListener("keyup", Game.keyup);
 
 		rawX = (posX * 48).toFixed();
 		rawY = (posY * 48).toFixed();
+
+		event.innerText = "No event";
 
 		block.aboveLeft = block.calcAboveLeft();
 		block.aboveRight = block.calcAboveRight();
@@ -142,58 +148,67 @@ const Game = {
 		block.belowLeft = block.calcBelowLeft();
 		block.belowRight = block.calcBelowRight();
 
-		if (block.leftTop === undefined && block.leftBottom === undefined) canMoveLeft = true;
-		if (block.rightTop === undefined && block.rightBottom === undefined) canMoveRight = true;
+		if (!isDefined(block.leftTop) && !isDefined(block.leftBottom)) canMoveLeft = true;
+		if (!isDefined(block.rightTop) && !isDefined(block.rightBottom)) canMoveRight = true;
 
-		// moving to left
+		// left movement event
 		if (dirLeft && canMoveLeft) {
 			canMoveRight = true;
-			posX -= speed;
+			posX -= Player.speed;
 			// sprite direction
-			player.style["-webkit-transform"] = "rotateY(180deg)";
-			player.style.transform = "rotateY(180deg)";
+			Player.sprite.style["-webkit-transform"] = "rotateY(180deg)";
+			Player.sprite.style.transform = "rotateY(180deg)";
 			// searching for collisions
-			if (posX <= 0) posX = 0; // map border
-			if (block.leftTop !== undefined || block.leftBottom !== undefined) {
+			if (posX <= 0) posX = 0; // border collision
+			if (isDefined(block.leftTop) || isDefined(block.leftBottom)) {
 				canMoveLeft = false;
 				if (posX % 1 !== 0) posX -= (posX % 1 - 1)
 			}
 		}
 
-		// moving to right
+		// right movement event
 		if (dirRight && canMoveRight) {
 			canMoveLeft = true;
-			posX += speed;
+			posX += Player.speed;
 			// sprite direction
-			player.style["-webkit-transform"] = "rotateY(0)";
-			player.style.transform = "rotateY(0)";
+			Player.sprite.style["-webkit-transform"] = "rotateY(0)";
+			Player.sprite.style.transform = "rotateY(0)";
 			// searching for collisions
-			if (posX >= mapBorder) posX = mapBorder; // map border
-			if (block.rightTop !== undefined || block.rightBottom !== undefined) {
+			if (posX >= lvlRightBorder) posX = lvlRightBorder; // border collision
+			if (isDefined(block.rightTop) || isDefined(block.rightBottom)) {
 				canMoveRight = false;
 				if (posX % 1 !== 0) posX -= (posX % 1)
 			}
 		}
 
-		// falling event
-		isFalling = (block.belowLeft === undefined && block.belowRight === undefined);
-		if (isFalling && !isJumping) {
-			event.innerText = "FALLING";
-			posY -= fallingSpeed;
-		} else {
-			canJump = true;
-			event.innerText = "No event"
-		}
-
-		// jumping event
-		canJump = (block.aboveLeft === undefined && block.aboveRight === undefined);
+		// jump event
+		canJump = (!isDefined(block.aboveLeft) && !isDefined(block.aboveRight));
 		if (canJump && isJumping) {
 			event.innerText = "JUMPING";
-			player.style.backgroundImage = "url(assets/entity/mario-jump.png)";
-			posY += fallingSpeed
-		} else {
-			event.innerText = "No event";
-			player.style.backgroundImage = "url(assets/entity/mario-idle.png)"
+			posY += Player.fallSpeed
+		}
+		if (isJumping) Player.sprite.style.backgroundImage = "url(assets/entity/mario-jump.png)";
+		else Player.sprite.style.backgroundImage = "url(assets/entity/mario-idle.png)";
+
+		// reach jump top event
+		/*if (isJumping && (block.aboveLeft !== undefined || block.aboveRight !== undefined)) {
+			event.innerText = "REACHED TOP";
+			isJumping = false;
+			canJump = false;
+			isFalling = true;
+		}*/
+
+		// fall event
+		isFalling = (!isJumping && !isDefined(block.belowLeft) && !isDefined(block.belowRight));
+		if (isFalling && !isJumping) {
+			event.innerText = "FALLING";
+			posY -= Player.fallSpeed;
+		}
+
+		if (isFalling && (isDefined(block.belowLeft) != isDefined(block.belowRight))) {
+			console.log(isFalling)
+			// canMoveRight = true;
+			// isFalling = false
 		}
 
 		// debug
@@ -201,16 +216,14 @@ const Game = {
 			`<u>PosX:</u> ${posX.toFixed()}<br>
 			<u>PosY:</u> ${posY.toFixed()}<br>
 			<u>RawX:</u> ${rawX}<br>
-			<u>RawY:</u> ${rawY}<br>
-			<u>Right Top:</u> ${block.rightTop}<br>
-			<u>Right Bottom:</u> ${block.rightBottom}<br>
-			<u>Can Jump:</u> ${canJump}<br>`;
+			<u>RawY:</u> ${rawY}<br>`;
 
-		player.style.bottom = `${rawY}px`;
-		player.style.left = `${rawX}px`;
+		Player.sprite.style.bottom = `${rawY}px`;
+		Player.sprite.style.left = `${rawX}px`;
 		Game.unfreeze()
 	},
 	freeze: function() {
+		// disable controls
 		if (canLoop) {
 			window.cancelAnimationFrame(canLoop);
 			canLoop = undefined
@@ -219,31 +232,37 @@ const Game = {
 		document.removeEventListener("keyup", Game.keyup)
 	}
 },
-speed = (6 / 48), // base speed
-fallingSpeed = (8 / 48), // base falling speed
+Player = {
+	sprite: document.querySelector(".player"), // player element
+	speed: (6 / 48), // base speed
+	fallSpeed: (8 / 48) // base fall speed
+},
+isDefined = function(e) {
+	// check if the targeted element is either empty or part of the environment
+	return !(e === 0 || e.includes("behind"))
+},
 block = {
+	// calculate collisions coming from above, left, right and below
 	aboveLeft: undefined,
 	calcAboveLeft: function() {
 		document.querySelectorAll(".map .above-left").forEach(function(e) {e.classList.remove("above-left")});
 		let request = [
-			Math.ceil(-posY + 7.5),
-			requestX = Math.floor(posX)
+			(-posY + 8.4).toFixed(),
+			Math.floor(posX)
 		];
-		try {
-			document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("above-left")
-		} catch (e) {}
+		try {document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("above-left")}
+		catch (e) {}
 		return lvl[request[0]][request[1]]
 	},
 	aboveRight: undefined,
 	calcAboveRight: function() {
 		document.querySelectorAll(".map .above-right").forEach(function(e) {e.classList.remove("above-right")});
 		let request = [
-			Math.ceil(-posY + 7.5),
-			requestX = Math.ceil(posX)
+			(-posY + 8.4).toFixed(),
+			Math.ceil(posX)
 		];
-		try {
-			document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("above-right")
-		} catch (e) {}
+		try {document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("above-right")}
+		catch (e) {}
 		return lvl[request[0]][request[1]]
 	},
 	leftTop: undefined,
@@ -251,11 +270,10 @@ block = {
 		document.querySelectorAll(".map .left-top").forEach(function(e) {e.classList.remove("left-top")});
 		let request = [
 			Math.floor(-posY + 9),
-			requestX = (posX - 0.6).toFixed()
+			(posX - 0.6).toFixed()
 		];
-		try {
-			document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("left-top")
-		} catch (e) {}
+		try {document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("left-top")}
+		catch (e) {}
 		return lvl[request[0]][request[1]]
 	},
 	leftBottom: undefined,
@@ -263,11 +281,10 @@ block = {
 		document.querySelectorAll(".map .left-bottom").forEach(function(e) {e.classList.remove("left-bottom")});
 		let request = [
 			Math.ceil(-posY + 8.9999),
-			requestX = (posX - 0.6).toFixed()
+			(posX - 0.6).toFixed()
 		];
-		try {
-			document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("left-bottom")
-		} catch (e) {}
+		try {document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("left-bottom")}
+		catch (e) {}
 		return lvl[request[0]][request[1]]
 	},
 	rightTop: undefined,
@@ -275,11 +292,10 @@ block = {
 		document.querySelectorAll(".map .right-top").forEach(function(e) {e.classList.remove("right-top")});
 		let request = [
 			Math.floor(-posY + 9),
-			requestX = (posX + 0.6).toFixed()
+			(posX + 0.6).toFixed()
 		];
-		try {
-			document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("right-top")
-		} catch (e) {}
+		try {document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("right-top")}
+		catch (e) {}
 		return lvl[request[0]][request[1]]
 	},
 	rightBottom: undefined,
@@ -287,11 +303,10 @@ block = {
 		document.querySelectorAll(".map .right-bottom").forEach(function(e) {e.classList.remove("right-bottom")});
 		let request = [
 			Math.ceil(-posY + 8.9999),
-			requestX = (posX + 0.6).toFixed()
+			(posX + 0.6).toFixed()
 		];
-		try {
-			document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("right-bottom")
-		} catch (e) {}
+		try {document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("right-bottom")}
+		catch (e) {}
 		return lvl[request[0]][request[1]]
 	},
 	belowLeft: undefined,
@@ -299,11 +314,10 @@ block = {
 		document.querySelectorAll(".map .below-left").forEach(function(e) {e.classList.remove("below-left")});
 		let request = [
 			(-posY + 9.5).toFixed(),
-			requestX = Math.floor(posX)
+			Math.floor(posX)
 		];
-		try {
-			document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("below-left")
-		} catch (e) {}
+		try {document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("below-left")}
+		catch (e) {}
 		return lvl[request[0]][request[1]]
 	},
 	belowRight: undefined,
@@ -311,53 +325,60 @@ block = {
 		document.querySelectorAll(".map .below-right").forEach(function(e) {e.classList.remove("below-right")});
 		let request = [
 			(-posY + 9.5).toFixed(),
-			requestX = Math.ceil(posX)
+			Math.ceil(posX)
 		];
-		try {
-			document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("below-right")
-		} catch (e) {}
+		try {document.querySelector(`.${lvl[request[0]][request[1]] + request[0] + request[1]}`).classList.add("below-right")}
+		catch (e) {}
 		return lvl[request[0]][request[1]]
 	}
 },
 map = document.querySelector(".map"),
 pause = document.querySelector(".pause"),
-debug = document.querySelector("#debug"),
-event = document.querySelector("#event"),
-A = undefined,
-G = "groundBlock",
-B = "brickBlock",
-M = "metalBlock",
-E = "emptyBlock",
-H = "hiddenBlock",
-$ = "mysteryBlock";
-let canLoop = false,
-dirLeft = false, // left direction
-canMoveLeft = true,
-dirRight = false, // right direction
-canMoveRight = true,
-canJump = true, // can do a jump
-isJumping = false, // is jumping
-isFalling = false, // is falling
-posX = 2, // X start coord (0-)
-posY = 2, // Y start coord (0-9)
-rawX, // X start coord (raw)
-rawY; // Y start coord (raw)
+debug = document.querySelector(".debug"),
+event = document.querySelector(".debug-event"),
+// terrain textures
+G = "ground",
+g = "ground behind",
+B = "brick",
+b = "brick behind",
+M = "metal",
+m = "metal behind",
+E = "empty",
+e = "empty behind",
+Y = "mystery",
+y = "mystery behind",
+H = "hidden";
+// coordinates, movement, jump, fall
+let canLoop, // looping function
+paused = true, // game paused
+spawned = false, // player spawn animation end
+dirLeft, // left direction
+canMoveLeft, // can move to left
+dirRight, // right direction
+canMoveRight, // can move to right
+canJump, // can do a jump
+isJumping, // is jumping
+isFalling, // is falling
+posX = 2, // X start coord (based on the number of elements in the level row)
+posY = 2, // Y start coord (based on the number of elements in the level column)
+rawX, // X start coord (raw, * 48)
+rawY; // Y start coord (raw, * 48)
 
 // init level
+// level pattern
 const lvl = [
-	// level pattern
-	[A, A, A, A, A, A, A, A, A, A, A, A, A],
-	[A, A, A, A, A, A, A, A, A, A, A, A, A],
-	[A, A, A, A, A, A, A, A, A, A, $, A, $],
-	[A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, M],
-	[A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, M, M],
-	[A, A, A, A, A, A, B, $, B, $, B, A, A, A, A, A, A, M, M, M],
-	[A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, M, M, M, M],
-	[A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, M, M, M, M, M],
-	[G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, E, 0, E, B, Y, B, Y],
+	[0, 0, 0, 0, 0, 0, M, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, M],
+	[0, 0, 0, 0, 0, 0, M, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, M, M],
+	[0, 0, 0, 0, 0, 0, 0, Y, B, Y, B, 0, 0, 0, 0, 0, 0, M, M, M, G, G, 0, 0, 0, 0, 0, G, G],
+	[0, 0, 0, 0, 0, 0, M, 0, 0, 0, 0, 0, 0, 0, 0, 0, M, M, M, M, G, G, 0, 0, 0, 0, 0, G, G],
+	[0, 0, 0, 0, 0, 0, M, 0, 0, M, 0, 0, 0, 0, 0, M, M, M, M, M, G, G, G, G, G, G, G, G, G],
+	[G, G, G, G, b, b, G, G, G, G, G, G, g, g, g, g, g, G, G, G, G, G, G, G, G, G, G, G, G],
 	[G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G, G]
 ],
-mapBorder = ((lvl[lvl.length - 1].length - 1) * 48);
+lvlRightBorder = (lvl[lvl.length - 1].length - 1); // equals to the length of the last row
 
 // load level
 Game.setLevelPattern(lvl);
@@ -367,4 +388,8 @@ Game.setEnvironment();
 document.addEventListener("keydown", function(e) {if (e.keyCode === 27) Game.togglePauseMenu()});
 pause.addEventListener("mousedown", Game.togglePauseMenu);
 
-const player = document.querySelector("div.player")
+// unfreeze after spawn animation
+setTimeout(function() {
+	spawned = true;
+	if (!paused) Game.unfreeze()
+}, 2000)
